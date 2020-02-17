13e2bf8825144c99670b49ce2d12137fde6a139d
Joachim Forsberg <jofo6666@hedenius.it.uu.se>
2020-02-17 13:04:49 +0100

    test localStorage

--- views/user/script/PPscript.js
+++ views/user/script/PPscript.js
@@ -8,9 +8,12 @@ const vm = new Vue({
   methods: {
     profileDone: function(){
       let namn = this.name;
+      localStorage.setItem("namn", namn);
       let ålder = this.age;
+      localStorage.setItem("ålder", ålder);
       if(namn.length > 0 && ålder > 0){
       let kön = this.gender;
+      localStorage.setItem("kön", kön);
       let profile = [namn,ålder,kön];
       for(let i = 0; i<3; i++){
         console.log(profile[i]);
