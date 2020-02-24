596fe5877ec0454843a3bd3cfd36ef7fce0ce271
jois6507 <johan.haag.6507@student.uu.se>
2020-02-24 19:23:12 +0100

    tested for multiple devices and fixed button and header for first profile page

--- public/js/questions.js
+++ public/js/questions.js
@@ -17,7 +17,7 @@ const vm = new Vue({
     },
     methods: {
         profileDone: async function() {
-            if(confirm("Ã„r du nÃ¶jd med dina svar?"))
+            if(confirm("Ãr du nöjd med dina svar?"))
             {
             localStorage.setItem("tobacco", this.tobacco);
             localStorage.setItem("question1", this.question1);
